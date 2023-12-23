7228298c34bfcf14ea87d1c5f95696f853cae0ad
Alan <tyujn24@mail.ru>
2023-12-23 15:18:52 +0300

    test bundle

--- app/shema/request_product.py
+++ app/shema/request_product.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship, mapped_column
 class RequestProductOrm(Base):
     __tablename__ = "request_product"
 
-    request_id = mapped_column(ForeignKey("request.id"), nullable=False,primary_key=True)
-    product_id = mapped_column(ForeignKey("product.id"), nullable=False,primary_key=True)
+
+    # request_id = mapped_column(ForeignKey("request.id"), nullable=False,primary_key=True)
+    # product_id = mapped_column(ForeignKey("product.id"), nullable=False,primary_key=True)
     count = Column(Integer, nullable=False, default=1, primary_key=True)
